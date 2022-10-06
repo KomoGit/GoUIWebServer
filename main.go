0ffd243be04f72f8ad8401e39fb1a810c8c124b8
James <entreur@gmail.com>
2022-10-07 00:52:30 +0400

    Added new todo's to the project.

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	"fyne.io/fyne/v2/widget"
 )
 
-var serverStarted = false
+// Manipulate the window settings here.
 var width float32 = 480
 var height float32 = 480
 
@@ -24,10 +24,22 @@ func main() {
 // TODO: Ensure the port input is only numbers. (Done)
 // Ensure that text of start server changes to stop server if server is already running.
 // Ensure that port panel cannot be left empty. (Done)
-// Allow user to change directory to one they wish. Using GUI.
+// Allow user to change directory to one they wish. Using GUI and commandline arguments.
+// Ensure the application doesn't close when facing an error (Specfically if the port is already in use.)
+
+/* Ensure that a link is generated for each html file found in static folder.
+# The urls should be automatically created without any additional modifications to the script itself.
+# Ensure that if the link doesn't exist, it goes straight to 404.
+*/
 
 // Current Issues; The GUI part of application freezes up when the server is running.
 // Logs do not get sent out to inform the user on success.
+
+func determineArgs(arg string) {
+	// Determine arguments given to the user. First one should be directory.
+	switch arg {
+	}
+}
 
 func SetControlPanel() { //GUI
 	a := app.New()
@@ -50,9 +62,8 @@ func SetControlPanel() { //GUI
 func receivePort(port string) {
 	if port != "" { //First if checks if port isn't empty.
 		if _, err := strconv.Atoi(port); err == nil { //Second if checks whether input is a number.
-			fmt.Printf("Trying port %q.\n", port)
+			fmt.Printf("Starting server at port %s.\n", port)
 			startServer(port)
-			serverStarted = true
 		} else {
 			fmt.Printf("%s is not a valid port.\n", port)
 		}
@@ -61,24 +72,11 @@ func receivePort(port string) {
 	}
 }
 
-func receiveDirectory() {
-
-}
-
 // Start server should also receive path string to determine lcoation of directory.
 func startServer(port string) {
 	fileServer := http.FileServer(http.Dir("./static")) //Directory that holds html files.
 	http.Handle("/", fileServer)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
-	}
-	if serverStarted {
-		fmt.Println("Success, server is active in port: %s" + port)
-	}
-}
-
-func determineArgs(arg string) {
-	// Determine arguments given to the user. First one should be directory.
-	switch arg {
 	}
 }
