d6a63a73734ee01f36f1d37f282f8f5d27ce1d64
James <entreur@gmail.com>
2022-10-08 20:06:07 +0400

    Made ExitButton fit DRY principle.

--- main.go
+++ main.go
@@ -18,8 +18,13 @@ var height float32 = 240
 
 //var serverRunning = false
 
+var stopAction = false
+
+// Global so other functions can access this as well.
 var a = app.New()
 var win = a.NewWindow("GoUIWebServer")
+
+var exitBtn = widget.NewButton("Exit Application", func() { os.Exit(0) }) //Fyne Button to shut app down.
 
 func main() {
 	SetControlPanel()
@@ -49,15 +54,13 @@ func main() {
 	Potential Fix:
 */
 
-func determineArgs(arg string) {
-	// Determine arguments given to the user. First one should be directory.
-	switch arg {
-	}
-}
+// func determineArgs(arg string) {
+// 	// Determine arguments given to the user. First one should be directory.
+// 	switch arg {
+// 	}
+// }
 
 func SetControlPanel() { //GUI
-	// a := app.New()
-	// win := a.NewWindow("GoUIWebServer")
 	win.SetFixedSize(true)
 	win.Resize(fyne.NewSize(width, height)) //When app launches the window will be in this dimensions.
 	//Fyne boilerplate code.
@@ -68,12 +71,20 @@ func SetControlPanel() { //GUI
 	//Multiple buttons can be and should be added to a single "content"
 	content := container.NewVBox(
 		input, widget.NewButton("Start Server", func() { receivePort(input.Text) }), //Start Server button.
-		widget.NewButton("Exit Application", func() { os.Exit(0) })) //Exit button.
+		exitBtn)
+	//widget.NewButton("Exit Application", func() { os.Exit(0) })) //Exit button.
 	win.SetContent(content)
 	win.ShowAndRun()
 }
-func ConfigureControlPanel(port string) {
+
+func ControlPanelStarted(port string) { //Control panel when the server is started.
 	win.SetTitle("GoUIWebServer - Running at: " + port)
+	content := container.NewVBox(
+		widget.NewButton("Stop Server", func() { stopActionController() }), //Start Server button.
+		exitBtn)
+	//widget.NewButton("Exit Application", func() { os.Exit(0) })) //Exit button. Should be moved into it's own container free from whether server is started or not.
+
+	win.SetContent(content)
 }
 
 // Perhaps this should be decoupled as well.
@@ -95,8 +106,20 @@ func receivePort(port string) {
 func startServer(port string) {
 	fileServer := http.FileServer(http.Dir("./static")) //Directory that holds html files.
 	http.Handle("/", fileServer)
-	ConfigureControlPanel(port)
+
+	ControlPanelStarted(port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
+	}
+}
+
+// // A terrible way to control applications start stop, but for now it will do.
+func stopActionController() {
+	if stopAction {
+		stopAction = false
+		log.Println("false")
+	} else {
+		stopAction = true
+		log.Println("true")
 	}
 }
